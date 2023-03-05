c3d0553d734f8b6ae337173c54f81c8720b65f29
cjeongmin <cjeongmin27@gmail.com>
2023-03-05 18:33:36 +0900

    feature: Content View -> Root View 수정

--- Haru/HaruApp.swift
+++ Haru/HaruApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct HaruApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
         }
     }
 }
---
+++ Haru/Views/RootView.swift
@@ -0,0 +1,21 @@
+//
+//  RootView.swift
+//  Haru
+//
+//  Created by 최정민 on 2023/03/05.
+//
+
+import Foundation
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text("Root View")
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
