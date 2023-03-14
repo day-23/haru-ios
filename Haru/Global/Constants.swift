a20d818c5898f79df07bac4ada16afe83426f5fc
leejh7 <777joonho@kookmin.ac.kr>
2023-03-14 16:52:29 +0900

    feature: Global, User 정의하기

---
+++ Haru/Global/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Haru
+//
+//  Created by 최정민 on 2023/03/08.
+//
+
+import Foundation
+import SwiftUI
+
+struct Constants {
+    static let dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    static let dateEncodingStrategy = JSONEncoder.DateEncodingStrategy.iso8601
+    static let dateDecodingStrategy = JSONDecoder.DateDecodingStrategy.iso8601
+    static let baseURL = "http://localhost:8000/"
+
+    static let gradientStart = Color(0x53acf8)
+    static let gradientEnd = Color(0x9da6f5)
+}
---
+++ Haru/Global/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  Haru
+//
+//  Created by 이준호 on 2023/03/14.
+//
+
+import Foundation
+
+final class {
+    
+}
