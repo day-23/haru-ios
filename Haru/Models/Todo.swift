515a0d2c53db32944062667dbbc5076073a516f3
cjeongmin <cjeongmin27@gmail.com>
2023-03-15 15:55:44 +0900

    fix: completed, order -> Nullable로 서버 문제로 임시 변경

--- Haru/Models/Todo.swift
+++ Haru/Models/Todo.swift
@@ -22,6 +22,7 @@ struct Todo: Identifiable, Codable {
     private(set) var endDateTime: Date?
     private(set) var repeatEnd: Date?
     private(set) var order: Int?
+    private(set) var completed: Bool?
     var subTodos: [SubTodo]
     private(set) var tags: [Tag]
 
