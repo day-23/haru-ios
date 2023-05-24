20860aef69aa4109f1c0377861726a0a11ef2061
cjeongmin <cjeongmin27@gmail.com>
2023-05-25 02:15:43 +0900

    feature: Haru Setting View 상세 내용 구현

--- Haru/Views/Setting/SettingView.swift
+++ Haru/Views/Setting/SettingView.swift
@@ -13,38 +13,82 @@ struct SettingView: View {
 
     var body: some View {
         VStack(spacing: 0) {
-            Divider()
-                .padding(.bottom, 19)
-            Spacer()
-
-            Button {
-                KeychainService.logout()
-                isLoggedIn = false
-            } label: {
-                Text("임시 로그아웃 (유저 정보 변경)")
-                    .font(.pretendard(size: 14, weight: .bold))
-                    .foregroundColor(Color(0xfdfdfd))
-                    .frame(width: 312, height: 44)
-                    .background(Color(0x191919))
-                    .cornerRadius(12)
-            }
-        }
-        .navigationBarBackButtonHidden()
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
+            HStack(spacing: 0) {
                 Button {
                     dismissAction.callAsFunction()
                 } label: {
                     Image("back-button")
                         .frame(width: 28, height: 28)
                 }
-            }
+                .padding(.leading, 20)
 
-            ToolbarItem(placement: .navigation) {
+                Spacer()
+            }
+            .overlay {
                 Text("설정")
                     .font(.pretendard(size: 20, weight: .bold))
                     .foregroundColor(Color(0x191919))
             }
+
+            Divider()
+                .padding(.top, 19)
+
+            VStack(spacing: 14) {
+                SettingRow(iconName: "account", content: "계정") {
+                    // TODO: 계정으로 연결
+                }
+
+                SettingRow(iconName: "privacy", content: "개인정보 보호") {
+                    // TODO: 개인정보 보호로 연결
+                }
+
+                SettingRow(iconName: "screen", content: "화면") {
+                    // TODO: 화면으로 연결
+                }
+
+                SettingRow(iconName: "alarm", content: "알림") {
+                    // TODO: 알림으로 연결
+                }
+
+                SettingRow(iconName: "information", content: "정보") {
+                    // TODO: 정보로 연결
+                }
+
+                SettingRow(iconName: "invite-friend", content: "친구 초대") {
+                    // TODO: 친구 초대로 연결
+                }
+            }
+            .padding(.top, 20)
+            .padding(.horizontal, 20)
+
+            Spacer()
+
+            VStack(alignment: .leading, spacing: 14) {
+                HStack(spacing: 0) {
+                    NavigationLink {} label: {
+                        Text("Version")
+                            .font(.pretendard(size: 14, weight: .regular))
+                            .foregroundColor(Color(0x646464))
+                    }
+                }.padding(.leading, 34)
+
+                Divider()
+
+                HStack(spacing: 0) {
+                    Button {
+                        KeychainService.logout()
+                        isLoggedIn = false
+                    } label: {
+                        Text("로그아웃")
+                            .font(.pretendard(size: 14, weight: .regular))
+                            .foregroundColor(Color(0x646464))
+                    }
+                }.padding(.leading, 34)
+
+                Divider()
+            }
+            .padding(.bottom, 64)
         }
+        .navigationBarBackButtonHidden()
     }
 }
