e95b5658736f4ff4cc8195e73ea92ae292dcfb4b
cjeongmin <cjeongmin27@gmail.com>
2023-05-28 17:33:12 +0900

    fix: Toggle Button 스타일 수정

--- Haru/Components/CustomToggleStyle.swift
+++ Haru/Components/CustomToggleStyle.swift
@@ -12,13 +12,18 @@ struct CustomToggleStyle: ToggleStyle {
         HStack {
             configuration.label
             Rectangle()
-                .fill(configuration.isOn ? Gradient(colors: [.gradientStart2, .gradientEnd2]) : Gradient(colors: [.gray, .gray]))
-                .frame(width: 38, height: 22, alignment: .center)
+                .fill(configuration.isOn
+                    ? LinearGradient(colors: [Color(0xD2D7FF), Color(0xAAD7FF)], startPoint: .leading, endPoint: .trailing)
+                    : LinearGradient(colors: [Color(0xDBDBDB)], startPoint: .leading, endPoint: .trailing))
+                .frame(width: 38, height: 18, alignment: .center)
                 .overlay(
                     Circle()
-                        .strokeBorder(configuration.isOn ? Color.gradientStart2 : Color.gray, lineWidth: 1)
-                        .background(Circle().fill(.white))
-                        .offset(x: configuration.isOn ? 8 : -8, y: 0)
+                        .strokeBorder(configuration.isOn
+                            ? LinearGradient(colors: [Color(0xD2D7FF), Color(0xAAD7FF)], startPoint: .leading, endPoint: .trailing)
+                            : LinearGradient(colors: [Color(0xDBDBDB)], startPoint: .leading, endPoint: .trailing),
+                            lineWidth: 1)
+                        .background(Circle().fill(Color(0xFDFDFD)))
+                        .offset(x: configuration.isOn ? 10 : -10, y: 0)
                 )
                 .cornerRadius(20)
                 .onTapGesture {
