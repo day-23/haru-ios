437ea5b36d90c4acf09ea82b8c51345f25bfa953
cjeongmin <cjeongmin27@gmail.com>
2023-05-29 17:52:58 +0900

    feature: Alarm Test 추가

--- Haru/HaruApp.swift
+++ Haru/HaruApp.swift
@@ -4,12 +4,43 @@
 //
 //  Created by 최정민 on 2023/03/25.
 //
+
 import KakaoSDKAuth
 import KakaoSDKCommon
 import SwiftUI
+import UserNotifications
+
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("[Debug] 알림 권한 획득")
+                Global.scheduleNotification(title: "테스트", body: "테스트", year: 2023, month: 5, day: 29, hour: 17, minute: 35, identifier: "haru-test")
+            } else {
+                print("[Debug] 알림 권한 거부")
+            }
+        }
+
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
+            -> Void
+    ) {
+        completionHandler([.banner, .list])
+    }
+}
 
 @main
 struct HaruApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @StateObject var global: Global = .shared
 
     init() {
