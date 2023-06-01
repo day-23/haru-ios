018999790e63ee1e38c1a4fe737af315a90dbdb8
cjeongmin <cjeongmin27@gmail.com>
2023-06-02 01:53:33 +0900

    fix: Todo 수정시, null 값 서버로 안보내지던 문제 해결

--- Haru/Models/Request.swift
+++ Haru/Models/Request.swift
@@ -49,6 +49,23 @@ struct Request: Codable {
                 "subTodosCompleted": self.subTodosCompleted as Any
             ]
         }
+
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: Request.Todo.CodingKeys.self)
+            try container.encode(self.content, forKey: Request.Todo.CodingKeys.content)
+            try container.encode(self.memo, forKey: Request.Todo.CodingKeys.memo)
+            try container.encode(self.todayTodo, forKey: Request.Todo.CodingKeys.todayTodo)
+            try container.encode(self.flag, forKey: Request.Todo.CodingKeys.flag)
+            try container.encode(self.endDate, forKey: Request.Todo.CodingKeys.endDate)
+            try container.encode(self.isAllDay, forKey: Request.Todo.CodingKeys.isAllDay)
+            try container.encode(self.alarms, forKey: Request.Todo.CodingKeys.alarms)
+            try container.encode(self.repeatOption, forKey: Request.Todo.CodingKeys.repeatOption)
+            try container.encode(self.repeatValue, forKey: Request.Todo.CodingKeys.repeatValue)
+            try container.encode(self.repeatEnd, forKey: Request.Todo.CodingKeys.repeatEnd)
+            try container.encode(self.tags, forKey: Request.Todo.CodingKeys.tags)
+            try container.encode(self.subTodos, forKey: Request.Todo.CodingKeys.subTodos)
+            try container.encode(self.subTodosCompleted, forKey: Request.Todo.CodingKeys.subTodosCompleted)
+        }
     }
 
     struct Schedule: Codable {
