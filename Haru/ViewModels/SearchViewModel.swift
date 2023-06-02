7d309183d016424690498cfa368ab3b3fd256a5b
leejh7 <777joonho@kookmin.ac.kr>
2023-06-03 05:00:55 +0900

    feature: 일정 검색을 위해 오늘에 가장 가까운 반복 일정 구하는 로직 구현

--- Haru/ViewModels/SearchViewModel.swift
+++ Haru/ViewModels/SearchViewModel.swift
@@ -36,6 +36,91 @@ final class SearchViewModel: ObservableObject {
             return schedule
         }
 
-        return schedule
+        if repeatValue.prefix(1) == "T" {
+            var day = 60 * 60 * 24
+            var repeatStart = schedule.repeatStart
+
+            while repeatStart < Date() {
+                do {
+                    repeatStart = try schedule.nextSucRepeatStartDate(curRepeatStart: repeatStart)
+                } catch {
+                    print("[Error] nextRepeatStartDate의 계산 오류 \(#file) \(#function)")
+                    return schedule
+                }
+            }
+
+            var repeatEnd = repeatStart.addingTimeInterval(
+                TimeInterval(
+                    Double(
+                        repeatValue.split(separator: "T")[0]
+                    ) ?? 0
+                )
+            )
+
+            var oriRepeatEnd: Date = repeatEnd
+            if repeatOption == RepeatOption.everyMonth.rawValue,
+               repeatStart.month != repeatEnd.month
+            {
+                repeatEnd = CalendarHelper.makeDate(date: repeatStart, hour: 23, minute: 55)
+            }
+
+            do {
+                var nextRepeatStart = try schedule.nextSucRepeatStartDate(curRepeatStart: repeatStart)
+                var prevRepeatEnd: Date
+                switch repeatOption {
+                case "매주":
+                    prevRepeatEnd = repeatEnd.addingTimeInterval(TimeInterval(-day * 7))
+                case "격주":
+                    prevRepeatEnd = repeatEnd.addingTimeInterval(TimeInterval(-day * 7 * 2))
+                case "매달":
+                    prevRepeatEnd = CalendarHelper.prevMonthDate(curDate: oriRepeatEnd)
+                case "매년":
+                    prevRepeatEnd = CalendarHelper.prevYearDate(curDate: oriRepeatEnd)
+                default:
+                    prevRepeatEnd = Date()
+                }
+
+                return Schedule.createRepeatSchedule(
+                    schedule: schedule,
+                    repeatStart: repeatStart,
+                    repeatEnd: repeatEnd,
+                    prevRepeatEnd: prevRepeatEnd,
+                    nextRepeatStart: nextRepeatStart
+                )
+
+            } catch {
+                print("[Error] nextSucRepeatStartDate의 계산 오류 \(#file) \(#function)")
+                return schedule
+            }
+
+        } else {
+            var repeatStart = schedule.repeatStart
+            var repeatEnd = CalendarHelper.makeDate(date: repeatStart, date2: schedule.repeatEnd)
+            while repeatStart < Date() {
+                do {
+                    repeatStart = try schedule.nextRepeatStartDate(curRepeatStart: repeatStart)
+                    repeatEnd = try schedule.nextRepeatStartDate(curRepeatStart: repeatEnd)
+                } catch {
+                    print("[Error] nextRepeatStartDate의 계산 오류 \(#file) \(#function)")
+                    return schedule
+                }
+            }
+
+            do {
+                var prevRepeatEnd = try schedule.prevRepeatEndDate(curRepeatEnd: repeatEnd)
+                var nextRepeatStart = try schedule.nextRepeatStartDate(curRepeatStart: repeatStart)
+
+                return Schedule.createRepeatSchedule(
+                    schedule: schedule,
+                    repeatStart: repeatStart,
+                    repeatEnd: repeatEnd,
+                    prevRepeatEnd: prevRepeatEnd,
+                    nextRepeatStart: nextRepeatStart
+                )
+            } catch {
+                print("[Error] nextRepeatStartDate / prevRepeatEndDate의 계산 오류 \(#file) \(#function)")
+                return schedule
+            }
+        }
     }
 }
