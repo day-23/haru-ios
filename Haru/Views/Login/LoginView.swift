c3412dfba362547b9179cd618b658f672212fda0
cjeongmin <cjeongmin27@gmail.com>
2023-06-12 22:14:05 +0900

    fix: KaKao Login 버튼 변경

--- Haru/Views/Login/LoginView.swift
+++ Haru/Views/Login/LoginView.swift
@@ -47,12 +47,20 @@ struct LoginView: View {
 
                     VStack(spacing: 18) {
                         // 카카오 로그인 이미지 품질 저하로 PSD 파일 변환 필요할 듯
-                        Image("kakao_login_medium_wide")
-                            .onTapGesture {
-                                authViewModel.handleKakaoLogin { isLoggedIn in
-                                    self.isLoggedIn = isLoggedIn
-                                }
+                        HStack(spacing: 6) {
+                            Image("kakao-login-logo")
+                            Text("카카오로 로그인하기")
+                                .font(.pretendard(size: 14, weight: .bold))
+                                .foregroundColor(.black)
+                        }
+                        .frame(width: 312, height: 44)
+                        .background(Color(0xfee500))
+                        .cornerRadius(12)
+                        .onTapGesture {
+                            authViewModel.handleKakaoLogin { isLoggedIn in
+                                self.isLoggedIn = isLoggedIn
                             }
+                        }
 
                         SignInWithAppleButton(isLoggedIn: $isLoggedIn)
                             .frame(width: 312, height: 44)
