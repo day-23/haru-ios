2e1265651bcf4dc79c49383218ff8a55b98ecdc0
leejh7 <777joonho@kookmin.ac.kr>
2023-06-13 02:31:55 +0900

    feature: 이미지 크롭을 위한 라이브러리 추가

---
+++ Haru/Components/CustomCropImagePicker.swift
@@ -0,0 +1,20 @@
+//
+//  CustomCropImagePicker.swift
+//  Haru
+//
+//  Created by 이준호 on 2023/06/13.
+//
+
+import SwiftUI
+
+struct CustomCropImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomCropImagePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomCropImagePicker()
+    }
+}
---
+++ Haru/Models/Crop.swift
@@ -0,0 +1,8 @@
+//
+//  Crop.swift
+//  Haru
+//
+//  Created by 이준호 on 2023/06/13.
+//
+
+import Foundation
